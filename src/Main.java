5a64cfecae4d5955cc36201f85291129d3d98ef7
olgacherry <zebroid81@yandex.ru>
2023-01-02 18:56:14 +0300

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        task1();
+    }
+        public static void task1() {
+
+
+        }
+
+
+        System.out.println("Hello world!");
+    }
+}
